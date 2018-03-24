90feac6f18d9934243f3693770d13c4a26e6f8bb
ankdesh <a.s.deshwal@gmail.com>
2018-03-24 05:10:35 +0000

    Fix error for tree traversal

--- python/ppa_tree.py
+++ python/ppa_tree.py
@@ -1,12 +1,13 @@
 from pprint import pprint, pformat
 from ppa_utils import verbose_print
+from graphviz import Graph
 
 class Node:
   ''' Node represents a node in the expression tree for 
       recursively calculating top level Value
   '''
   
-  def __init__(self, name, isLeaf, valuePerInstance = None, numInstances = None):
+  def __init__(self, name, isLeaf, nodeValue = None, counter = None):
 
     ''' 
       Attributes:
@@ -14,19 +15,19 @@ class Node:
       children: list of children nodes
       if Leaf: Boolean to indicate if this is basic block node
       value: value of the parameter to be calculated
-      valuePerInstance: Value of each instance of node (e.g. area of BB)
-      numInstance: number of instances of the block
+      nodeValue: Fixed value of each instance of node (e.g. area of BB)
+      counter: number of events for the node (e.g. num of BB for area)
     '''
 
     self.name = name
     self.children = []
     self.isLeaf = isLeaf
-    self.valuePerInstance = valuePerInstance
-    self.numInstances = numInstances
+    self.nodeValue = nodeValue
+    self.counter = counter
     self.value = None
 
-    if self.valuePerInstance != None and self.numInstances != None:
-      self.value = self.valuePerInstance * self.numInstances
+    if self.nodeValue != None and self.counter != None:
+      self.value = self.nodeValue * self.counter
 
 
   def addChild(self, child):
@@ -54,16 +55,14 @@ class Node:
  
     #self.printNode() 
     for child in self.children:
-      print ('>>' + child.name)
       child.updateTreeValue()
-      
-    childVals = [x.value for x in self.children]
+    
+    if not self.isLeaf:  
+      childVals = [x.value for x in self.children]
+      for i, val in enumerate(childVals):
+        assert val != None, self.children[i].name + ' not complete '
 
-    print ('>>>> ' + str(childVals))
-    #for i, val in enumerate(childVals):
-    #  assert val != None, self.children[i].name + ' not complete '
-
-    self.value = sum(childVals)
+      self.value = sum(childVals)
 
   def printNode(self):
     ''' Prints the Node info with all attributes
@@ -95,6 +94,43 @@ class Node:
 
     for child in self.children:
       child.printTreePreorder(full)
+  
+  def _constructDotNodes(self, G, full = 0):
+    ''' Add nodes to Dot Graph
+    '''
+  
+    if full == 0: 
+      desc = self.name + '\n' + str(self.value)
+    else:
+      desc = pformat(self.__dict__)
+
+    # Create Node
+    G.node(self.name, label = desc)
+
+    for child in self.children:
+      child._constructDotNodes(G,full)
+
+  def _constructDotEdges(self, G, full = 0):
+    ''' Add edges to Dot Graph
+    '''
+    for child in self.children:
+      G.edge(self.name, child.name)
+      child._constructDotEdges(G,full)
+    
+
+  def dumpGraphPng(self, filename ='graph', full = 0):
+    ''' Dumps a png image for  entire tree rooted at this node
+
+        Args:
+        G: Dot Graph
+        full: 0 prints only node names
+              1 prints full node info
+    '''
+    G = Graph('ppa', format='png')
+    self._constructDotNodes(G,full)
+    self._constructDotEdges(G,full)
+
+    G.render(filename, cleanup = True)
     
 
 if __name__ == "__main__":
@@ -116,3 +152,5 @@ if __name__ == "__main__":
   node0.updateTreeValue()
 
   node0.printTreePreorder(1)
+
+  node0.dumpGraphPng('graph',0)
